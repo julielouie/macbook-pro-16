f28d1fd56a35d0a61c34888f002482749c2347dc
Julie Chung <juliehchung96@gmail.com>
2020-03-13 19:19:41 +0000

    Finalized basic hero styling

--- client/components/hero.jsx
+++ client/components/hero.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Hero(props) {
   return (
     <div className="container vh-100">
-      <div className="hero-img text-center">
+      <div className="hero-img text-center m-auto">
         <h2 className="title">MacBook Pro</h2>
-        <h1 className="tagline">The best for the brightest.</h1>
+        <h1 className="tagline m-auto">The best for the brightest.</h1>
       </div>
     </div>
   );
