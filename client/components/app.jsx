9d9dcfd35d2d2d173d3b91142d207f93734e9269
Julie Chung <juliehchung96@gmail.com>
2020-03-13 19:44:47 +0000

    Added basic summary with styling

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from './hero';
+import FilmSummary from './film-summary';
 
 class App extends React.Component {
   render() {
     return (
-      <Hero />
+      <>
+        <Hero />
+        <FilmSummary />
+      </>
     );
   }
 }
---
+++ client/components/film-summary.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function FilmSummary(props) {
+  return (
+    <div className="container vh-100">
+      <h2 className="summary text-muted">
+        Designed for those who defy limits and change the world,
+        the new MacBook Pro is by far the most powerful notebook we’ve ever made.
+        With an immersive 16-inch Retina display, superfast processors, next-generation graphics,
+        the largest battery capacity ever in a MacBook Pro,
+        a new Magic Keyboard, and massive storage,
+        it’s the ultimate pro notebook for the ultimate user.
+      </h2>
+      <h4 className="video">Watch the film <img src="./public_assets/play-button.svg" alt="Play Button" className="play" />
+      </h4>
+    </div>
+  );
+}
+
+export default FilmSummary;
