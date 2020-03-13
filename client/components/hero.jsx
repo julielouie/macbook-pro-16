0eff583a760a156dee56d2013fc95721146f13fd
Julie Chung <juliehchung96@gmail.com>
2020-03-13 18:50:32 +0000

    Basic frame for hero component complete

---
+++ client/components/hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero(props) {
+  return (
+    <div className="container vh-100">
+      <div className="hero-img text-center">
+        <h1 className="title">MacBook Pro</h1>
+        <h2 className="tagline">The best for the brightest.</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
