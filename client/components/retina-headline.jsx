ff3bada38155c14dc563a09195e511c2b56326de
Julie Chung <juliehchung96@gmail.com>
2020-03-15 04:05:02 +0000

    Basic styling for retina-headline completed

--- client/components/retina-headline.jsx
+++ client/components/retina-headline.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 function RetinaHeadline(props) {
   return (
     <div className="position-relative pt-5">
-      <div className="container pt-5">
-        <h1 className="retina col-12">
-          <p className="text-muted">Retina Display</p>
-          A big, beautiful workspace.
+      <div className="col-10 py-5 m-auto">
+        <h1 className="retina m-auto">
+          <p className="text-muted m-auto">Retina Display</p>
+            A big, beautiful workspace.
           <p>For doing big, beautiful work.</p>
         </h1>
       </div>
-      <div className="ret-img-content h-100 mt-5 pt-5">
-        <div className="retina-img-1 col-md-10"></div>
-        <div className="retina-quote col-md-5 .d-none .d-xl-none">
+      <div className="ret-img-content col-md-10 h-100 pt-5 m-auto">
+        <div className="retina-img-1"></div>
+        <div className="retina-quote col-md-6 .d-none .d-xl-none">
           <h1 className="retina-quote-initial">Boundaries are meant to be pushed.</h1>
           <div className="retina-quote-2">
             <div className="retina-quote-author">Chris Burkard</div>
