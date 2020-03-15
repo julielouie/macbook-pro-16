974457aa9ce1dae1e38563f24e8a4d0b7cd0ecba
Julie Chung <juliehchung96@gmail.com>
2020-03-15 03:44:15 +0000

    Finalized initial styling for desktop view retina img 1

--- client/components/retina-headline.jsx
+++ client/components/retina-headline.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 function RetinaHeadline(props) {
   return (
-    <div className="position-relative">
-      <div className="container">
+    <div className="position-relative pt-5">
+      <div className="container pt-5">
         <h1 className="retina col-12">
           <p className="text-muted">Retina Display</p>
           A big, beautiful workspace.
           <p>For doing big, beautiful work.</p>
         </h1>
       </div>
-      <div className="ret-img-content h-100">
-        <div className="retina-img-1 col-md-8"></div>
-        <div className="retina-quote .d-none .d-xl-none">
-          Boundaries are meant to be pushed.
-          <span>Chris Burkard</span>
-          <span>Adventure Photographer</span>
+      <div className="ret-img-content h-100 mt-5 pt-5">
+        <div className="retina-img-1 col-md-10"></div>
+        <div className="retina-quote col-md-5 .d-none .d-xl-none">
+          <h1 className="retina-quote-initial">Boundaries are meant to be pushed.</h1>
+          <div className="retina-quote-2">
+            <div className="retina-quote-author">Chris Burkard</div>
+            <div>Adventure Photographer</div>
+          </div>
         </div>
       </div>
       <div className="retina-img-2"></div>
