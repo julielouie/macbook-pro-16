b9e1f507d7861231dff3b389c223c0c0c02eba67
Julie Chung <juliehchung96@gmail.com>
2020-03-15 23:17:48 +0000

    Modifying to accomodate responsiveness of hero-component

--- client/components/hero.jsx
+++ client/components/hero.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 function Hero(props) {
   return (
-    <div className="container vh-100">
-      <div className="hero-img text-center m-auto">
-        <h2 className="title">MacBook Pro</h2>
-        <h1 className="tagline m-auto">The best for the brightest.</h1>
+    <div className="col-10 m-auto vh-100 position-relative">
+      <div className="hero-text pt-5 m-auto">
+        <h2 className="title text-center">MacBook Pro</h2>
+        <h1 className="tagline text-center">
+          The best for
+          <span className="tagline break">the brightest.</span>
+        </h1>
       </div>
+      <div className="hero-img m-auto"></div>
     </div>
   );
 }
