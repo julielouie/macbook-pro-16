1592913e08b143a364b4d728f269c49abf7d97d9
Julie Chung <juliehchung96@gmail.com>
2020-03-15 01:27:55 +0000

    First retina image sized correctly, working on quote

--- client/components/retina-headline.jsx
+++ client/components/retina-headline.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 function RetinaHeadline(props) {
   return (
-    <div className="container vw-100 vh-100">
-      <div className="retina text-muted">Retina Display</div>
-      <div className="retina-description">
-        A big, beautiful workspace.
-        <span>For doing big, beautiful work.</span>
+    <div className="retina-headline">
+      <div className="container">
+        <h1 className="retina col-12">
+          <p className="text-muted">Retina Display</p>
+          A big, beautiful workspace.
+          <p>For doing big, beautiful work.</p>
+        </h1>
       </div>
-      <div className="retina-img-1"></div>
+      <div className="ret-img-content h-100">
+        <div className="retina-quote .d-none .d-md-block .d-xl-none">
+          Boundaries are meant to be pushed.
+          Chris Burkard
+          <span>Adventure Photographer</span>
+        </div>
+        <div className="retina-img-1 col-md-8"></div>
+      </div>
       <div className="retina-img-2"></div>
     </div>
   );
