916aac2f44b11a071193fdaae365c73cb49eaf0a
Julie Chung <juliehchung96@gmail.com>
2020-03-18 02:25:48 +0000

    Figured out attempted image sequence

--- client/components/hero.jsx
+++ client/components/hero.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Controller, Scene } from 'react-scrollmagic';
+import Sequence from './sequence/index';
 
 function Hero(props) {
+  const ref = useRef();
   return (
     <div className="hero col-12 col-md-10 m-auto position-relative">
       <div className="hero-text position-absolute pt-5 w-100 d-flex flex-column text-center">
@@ -10,7 +13,16 @@ function Hero(props) {
           <span className="tagline break">the brightest.</span>
         </h1>
       </div>
-      <div className="hero-img m-auto"></div>
+      <Controller>
+        <Scene duration="200%" triggerHook="onLeave" pin>
+          {progress => (
+            <div style={{ height: '100vh', position: 'relative' }}>
+              <Sequence ref={ref} progress={progress} />
+            </div>
+          )}
+        </Scene>
+      </Controller>
+      {/* <div className="hero-img m-auto"></div> */}
     </div>
   );
 }
---
+++ client/components/sequence/image-array.js
@@ -0,0 +1,303 @@
+import { MacBookPro16 } from '../../../server/public/assets/sequence-images';
+import { useImage } from './hooks';
+
+const MacBookPro16Array = () => {
+  const [AA0, AA0S] = useImage(MacBookPro16[0]);
+  const [AA1, AA1S] = useImage(MacBookPro16[1]);
+  const [AA2, AA2S] = useImage(MacBookPro16[2]);
+  const [AA3, AA3S] = useImage(MacBookPro16[3]);
+  const [AA4, AA4S] = useImage(MacBookPro16[4]);
+  const [AA5, AA5S] = useImage(MacBookPro16[5]);
+  const [AA6, AA6S] = useImage(MacBookPro16[6]);
+  const [AA7, AA7S] = useImage(MacBookPro16[7]);
+  const [AA8, AA8S] = useImage(MacBookPro16[8]);
+  const [AA9, AA9S] = useImage(MacBookPro16[9]);
+
+  const [AA10, AA10S] = useImage(MacBookPro16[10]);
+  const [AA11, AA11S] = useImage(MacBookPro16[11]);
+  const [AA12, AA12S] = useImage(MacBookPro16[12]);
+  const [AA13, AA13S] = useImage(MacBookPro16[13]);
+  const [AA14, AA14S] = useImage(MacBookPro16[14]);
+  const [AA15, AA15S] = useImage(MacBookPro16[15]);
+  const [AA16, AA16S] = useImage(MacBookPro16[16]);
+  const [AA17, AA17S] = useImage(MacBookPro16[17]);
+  const [AA18, AA18S] = useImage(MacBookPro16[18]);
+  const [AA19, AA19S] = useImage(MacBookPro16[19]);
+
+  const [AA20, AA20S] = useImage(MacBookPro16[20]);
+  const [AA21, AA21S] = useImage(MacBookPro16[21]);
+  const [AA22, AA22S] = useImage(MacBookPro16[22]);
+  const [AA23, AA23S] = useImage(MacBookPro16[23]);
+  const [AA24, AA24S] = useImage(MacBookPro16[24]);
+  const [AA25, AA25S] = useImage(MacBookPro16[25]);
+  const [AA26, AA26S] = useImage(MacBookPro16[26]);
+  const [AA27, AA27S] = useImage(MacBookPro16[27]);
+  const [AA28, AA28S] = useImage(MacBookPro16[28]);
+  const [AA29, AA29S] = useImage(MacBookPro16[29]);
+
+  const [AA30, AA30S] = useImage(MacBookPro16[30]);
+  const [AA31, AA31S] = useImage(MacBookPro16[31]);
+  const [AA32, AA32S] = useImage(MacBookPro16[32]);
+  const [AA33, AA33S] = useImage(MacBookPro16[33]);
+  const [AA34, AA34S] = useImage(MacBookPro16[34]);
+  const [AA35, AA35S] = useImage(MacBookPro16[35]);
+  const [AA36, AA36S] = useImage(MacBookPro16[36]);
+  const [AA37, AA37S] = useImage(MacBookPro16[37]);
+  const [AA38, AA38S] = useImage(MacBookPro16[38]);
+  const [AA39, AA39S] = useImage(MacBookPro16[39]);
+
+  const [AA40, AA40S] = useImage(MacBookPro16[40]);
+  const [AA41, AA41S] = useImage(MacBookPro16[41]);
+  const [AA42, AA42S] = useImage(MacBookPro16[42]);
+  const [AA43, AA43S] = useImage(MacBookPro16[43]);
+  const [AA44, AA44S] = useImage(MacBookPro16[44]);
+  const [AA45, AA45S] = useImage(MacBookPro16[45]);
+  const [AA46, AA46S] = useImage(MacBookPro16[46]);
+  const [AA47, AA47S] = useImage(MacBookPro16[47]);
+  const [AA48, AA48S] = useImage(MacBookPro16[48]);
+  const [AA49, AA49S] = useImage(MacBookPro16[49]);
+
+  const [AA50, AA50S] = useImage(MacBookPro16[50]);
+  const [AA51, AA51S] = useImage(MacBookPro16[51]);
+  const [AA52, AA52S] = useImage(MacBookPro16[52]);
+  const [AA53, AA53S] = useImage(MacBookPro16[53]);
+  const [AA54, AA54S] = useImage(MacBookPro16[54]);
+  const [AA55, AA55S] = useImage(MacBookPro16[55]);
+  const [AA56, AA56S] = useImage(MacBookPro16[56]);
+  const [AA57, AA57S] = useImage(MacBookPro16[57]);
+  const [AA58, AA58S] = useImage(MacBookPro16[58]);
+  const [AA59, AA59S] = useImage(MacBookPro16[59]);
+
+  const [AA60, AA60S] = useImage(MacBookPro16[60]);
+  const [AA61, AA61S] = useImage(MacBookPro16[61]);
+  const [AA62, AA62S] = useImage(MacBookPro16[62]);
+  const [AA63, AA63S] = useImage(MacBookPro16[63]);
+  const [AA64, AA64S] = useImage(MacBookPro16[64]);
+  const [AA65, AA65S] = useImage(MacBookPro16[65]);
+  const [AA66, AA66S] = useImage(MacBookPro16[66]);
+  const [AA67, AA67S] = useImage(MacBookPro16[67]);
+  const [AA68, AA68S] = useImage(MacBookPro16[68]);
+  const [AA69, AA69S] = useImage(MacBookPro16[69]);
+
+  const [AA70, AA70S] = useImage(MacBookPro16[70]);
+  const [AA71, AA71S] = useImage(MacBookPro16[71]);
+  const [AA72, AA72S] = useImage(MacBookPro16[72]);
+  const [AA73, AA73S] = useImage(MacBookPro16[73]);
+  const [AA74, AA74S] = useImage(MacBookPro16[74]);
+  const [AA75, AA75S] = useImage(MacBookPro16[75]);
+  const [AA76, AA76S] = useImage(MacBookPro16[76]);
+  const [AA77, AA77S] = useImage(MacBookPro16[77]);
+  const [AA78, AA78S] = useImage(MacBookPro16[78]);
+  const [AA79, AA79S] = useImage(MacBookPro16[79]);
+
+  const [AA80, AA80S] = useImage(MacBookPro16[80]);
+  const [AA81, AA81S] = useImage(MacBookPro16[81]);
+  const [AA82, AA82S] = useImage(MacBookPro16[82]);
+  const [AA83, AA83S] = useImage(MacBookPro16[83]);
+  const [AA84, AA84S] = useImage(MacBookPro16[84]);
+  const [AA85, AA85S] = useImage(MacBookPro16[85]);
+  const [AA86, AA86S] = useImage(MacBookPro16[86]);
+  const [AA87, AA87S] = useImage(MacBookPro16[87]);
+  const [AA88, AA88S] = useImage(MacBookPro16[88]);
+  const [AA89, AA89S] = useImage(MacBookPro16[89]);
+
+  const [AA90, AA90S] = useImage(MacBookPro16[90]);
+  const [AA91, AA91S] = useImage(MacBookPro16[91]);
+  const [AA92, AA92S] = useImage(MacBookPro16[92]);
+  const [AA93, AA93S] = useImage(MacBookPro16[93]);
+  const [AA94, AA94S] = useImage(MacBookPro16[94]);
+  const [AA95, AA95S] = useImage(MacBookPro16[95]);
+  const [AA96, AA96S] = useImage(MacBookPro16[96]);
+  const [AA97, AA97S] = useImage(MacBookPro16[97]);
+  const [AA98, AA98S] = useImage(MacBookPro16[98]);
+  const [AA99, AA99S] = useImage(MacBookPro16[99]);
+
+  const [AA100, AA100S] = useImage(MacBookPro16[100]);
+  const [AA101, AA101S] = useImage(MacBookPro16[101]);
+  const [AA102, AA102S] = useImage(MacBookPro16[102]);
+  const [AA103, AA103S] = useImage(MacBookPro16[103]);
+  const [AA104, AA104S] = useImage(MacBookPro16[104]);
+  const [AA105, AA105S] = useImage(MacBookPro16[105]);
+  const [AA106, AA106S] = useImage(MacBookPro16[106]);
+  const [AA107, AA107S] = useImage(MacBookPro16[107]);
+  const [AA108, AA108S] = useImage(MacBookPro16[108]);
+  const [AA109, AA109S] = useImage(MacBookPro16[109]);
+
+  const [AA110, AA110S] = useImage(MacBookPro16[110]);
+  const [AA111, AA111S] = useImage(MacBookPro16[111]);
+  const [AA112, AA112S] = useImage(MacBookPro16[112]);
+  const [AA113, AA113S] = useImage(MacBookPro16[113]);
+  const [AA114, AA114S] = useImage(MacBookPro16[114]);
+  const [AA115, AA115S] = useImage(MacBookPro16[115]);
+  const [AA116, AA116S] = useImage(MacBookPro16[116]);
+  const [AA117, AA117S] = useImage(MacBookPro16[117]);
+  const [AA118, AA118S] = useImage(MacBookPro16[118]);
+  const [AA119, AA119S] = useImage(MacBookPro16[119]);
+
+  const [AA120, AA120S] = useImage(MacBookPro16[120]);
+  const [AA121, AA121S] = useImage(MacBookPro16[121]);
+  const [AA122, AA122S] = useImage(MacBookPro16[122]);
+  const [AA123, AA123S] = useImage(MacBookPro16[123]);
+  const [AA124, AA124S] = useImage(MacBookPro16[124]);
+  const [AA125, AA125S] = useImage(MacBookPro16[125]);
+  const [AA126, AA126S] = useImage(MacBookPro16[126]);
+  const [AA127, AA127S] = useImage(MacBookPro16[127]);
+  const [AA128, AA128S] = useImage(MacBookPro16[128]);
+  const [AA129, AA129S] = useImage(MacBookPro16[129]);
+
+  const [AA130, AA130S] = useImage(MacBookPro16[130]);
+  const [AA131, AA131S] = useImage(MacBookPro16[131]);
+  const [AA132, AA132S] = useImage(MacBookPro16[132]);
+  const [AA133, AA133S] = useImage(MacBookPro16[133]);
+
+  const newImages = Array.of(
+    [AA0, AA0S],
+    [AA1, AA1S],
+    [AA2, AA2S],
+    [AA3, AA3S],
+    [AA4, AA4S],
+    [AA5, AA5S],
+    [AA6, AA6S],
+    [AA7, AA7S],
+    [AA8, AA8S],
+    [AA9, AA9S],
+    [AA10, AA10S],
+    [AA11, AA11S],
+    [AA12, AA12S],
+    [AA13, AA13S],
+    [AA14, AA14S],
+    [AA15, AA15S],
+    [AA16, AA16S],
+    [AA17, AA17S],
+    [AA18, AA18S],
+    [AA19, AA19S],
+    [AA20, AA20S],
+    [AA21, AA21S],
+    [AA22, AA22S],
+    [AA23, AA23S],
+    [AA24, AA24S],
+    [AA25, AA25S],
+    [AA26, AA26S],
+    [AA27, AA27S],
+    [AA28, AA28S],
+    [AA29, AA29S],
+
+    [AA30, AA30S],
+    [AA31, AA31S],
+    [AA32, AA32S],
+    [AA33, AA33S],
+    [AA34, AA34S],
+    [AA35, AA35S],
+    [AA36, AA36S],
+    [AA37, AA37S],
+    [AA38, AA38S],
+    [AA39, AA39S],
+
+    [AA40, AA40S],
+    [AA41, AA41S],
+    [AA42, AA42S],
+    [AA43, AA43S],
+    [AA44, AA44S],
+    [AA45, AA45S],
+    [AA46, AA46S],
+    [AA47, AA47S],
+    [AA48, AA48S],
+    [AA49, AA49S],
+
+    [AA50, AA50S],
+    [AA51, AA51S],
+    [AA52, AA52S],
+    [AA53, AA53S],
+    [AA54, AA54S],
+    [AA55, AA55S],
+    [AA56, AA56S],
+    [AA57, AA57S],
+    [AA58, AA58S],
+    [AA59, AA59S],
+
+    [AA60, AA60S],
+    [AA61, AA61S],
+    [AA62, AA62S],
+    [AA63, AA63S],
+    [AA64, AA64S],
+    [AA65, AA65S],
+    [AA66, AA66S],
+    [AA67, AA67S],
+    [AA68, AA68S],
+    [AA69, AA69S],
+
+    [AA70, AA70S],
+    [AA71, AA71S],
+    [AA72, AA72S],
+    [AA73, AA73S],
+    [AA74, AA74S],
+    [AA75, AA75S],
+    [AA76, AA76S],
+    [AA77, AA77S],
+    [AA78, AA78S],
+    [AA79, AA79S],
+
+    [AA80, AA80S],
+    [AA81, AA81S],
+    [AA82, AA82S],
+    [AA83, AA83S],
+    [AA84, AA84S],
+    [AA85, AA85S],
+    [AA86, AA86S],
+    [AA87, AA87S],
+    [AA88, AA88S],
+    [AA89, AA89S],
+
+    [AA90, AA90S],
+    [AA91, AA91S],
+    [AA92, AA92S],
+    [AA93, AA93S],
+    [AA94, AA94S],
+    [AA95, AA95S],
+    [AA96, AA96S],
+    [AA97, AA97S],
+    [AA98, AA98S],
+    [AA99, AA99S],
+
+    [AA100, AA100S],
+    [AA101, AA101S],
+    [AA102, AA102S],
+    [AA103, AA103S],
+    [AA104, AA104S],
+    [AA105, AA105S],
+    [AA106, AA106S],
+    [AA107, AA107S],
+    [AA108, AA108S],
+    [AA109, AA109S],
+
+    [AA110, AA110S],
+    [AA111, AA111S],
+    [AA112, AA112S],
+    [AA113, AA113S],
+    [AA114, AA114S],
+    [AA115, AA115S],
+    [AA116, AA116S],
+    [AA117, AA117S],
+    [AA118, AA118S],
+    [AA119, AA119S],
+
+    [AA120, AA120S],
+    [AA121, AA121S],
+    [AA122, AA122S],
+    [AA123, AA123S],
+    [AA124, AA124S],
+    [AA125, AA125S],
+    [AA126, AA126S],
+    [AA127, AA127S],
+    [AA128, AA128S],
+    [AA129, AA129S],
+
+    [AA130, AA130S],
+    [AA131, AA131S],
+    [AA132, AA132S],
+    [AA133, AA133S]
+  );
+  return newImages;
+};
+
+export default MacBookPro16Array;
