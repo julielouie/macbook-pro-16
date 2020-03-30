523459b73bf3f665170bc05e21ba77feff7e0196
Julie Chung <juliehchung96@gmail.com>
2020-03-30 20:23:50 +0000

    Successful video play on scroll

--- client/components/animations/animated-video.js
+++ client/components/animations/animated-video.js
@@ -5,8 +5,12 @@ function AnimatedVideo({ progress }, ref) {
   // const matrix = progress === 1 ? 0 : 40 - (progress * 40);
   // const opacity = progress === 1 ? 1 : expedite - 1;
 
+  if (progress === 1) {
+    document.querySelector('#specVid').play();
+  }
+
   const display = window.innerWidth > 767
-    ? <video ref={ref} width="100%" height="100%" autoPlay>
+    ? <video ref={ref} width="100%" height="100%" id="specVid">
       <source src="./assets/media/large.mp4" type="video/mp4" />
     </video>
     : <div className="specs-img-end" style={{
