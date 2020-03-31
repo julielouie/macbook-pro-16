81d5a98af8f166738626914b9791ab634111190e
Julie Chung <juliehchung96@gmail.com>
2020-03-31 19:27:29 +0000

    Refined spec text animations

--- client/components/animations/animated-text-specs.js
+++ client/components/animations/animated-text-specs.js
@@ -1,12 +1,13 @@
 import React, { forwardRef } from 'react';
 
 function AnimatedTextSpecs({ progress, specText }, ref) {
-  const expedite = progress * 3;
-  const matrix = progress === 1 ? 0 : 40 - (progress * 40);
+  const expedite = progress * 2;
+  const matrix = progress === 1 ? 0 : 50 - (progress * 50);
   const opacity = progress === 1 ? 1 : expedite - 1;
 
   const displayMed = specText[0].includes('AMD') ? 'd-block d-sm-block d-md-none' : null;
   const displaySm = specText[0].includes('Six-speaker') ? 'd-none' : null;
+
   return (
     <div
       ref={ref}
